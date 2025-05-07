cf4702144420160835afc0a5062c232bd406924b
Nicolas Corbalan <nicolascorbalan@gmail.com>
2025-05-07 18:00:26 +0000

    hash a promos_id

--- models/staging/sql_server_dbo/stg_addresses.sql
+++ models/staging/sql_server_dbo/stg_addresses.sql
@@ -1,24 +1,19 @@
-{{ config(materialized = 'view') }}
+{{ config(materialized='view') }}
 
 with source as (
-
-    select * 
-    from {{ source('sql_server_dbo', 'addresses') }}
-
+  select * from {{ source('sql_server_dbo', 'addresses') }}
 ),
 
-cleaned as (
-
-    select
-        address_id,
-        address,
-        zipcode,
-        country,
-        state,
-        _fivetran_synced
-    from source
-    where _fivetran_deleted is null or _fivetran_deleted = false
-
+renamed as (
+  select
+    address_id::string as address_id,
+    zipcode::int as zipcode,
+    country::string as country,
+    address::string as address,
+    state::string as state,
+    _fivetran_synced::timestamp as _fivetran_synced
+  from source
+  where coalesce(_fivetran_deleted, false) = false
 )
 
-select * from cleaned
+select * from renamed
--- models/staging/sql_server_dbo/stg_events.sql
+++ models/staging/sql_server_dbo/stg_events.sql
@@ -1,27 +1,22 @@
-{{ config(materialized = 'view') }}
+{{ config(materialized='view') }}
 
 with source as (
-
-    select * 
-    from {{ source('sql_server_dbo', 'events') }}
-
+  select * from {{ source('sql_server_dbo', 'events') }}
 ),
 
-cleaned as (
-
-    select
-        event_id,
-        event_type,
-        page_url,
-        product_id,
-        user_id,
-        session_id,
-        created_at,
-        order_id,
-        _fivetran_synced
-    from source
-    where _fivetran_deleted is null or _fivetran_deleted = false
-
+renamed as (
+  select
+    event_id::string as event_id,
+    page_url::string as page_url,
+    event_type::string as event_type,
+    user_id::string as user_id,
+    product_id::string as product_id,
+    session_id::string as session_id,
+    created_at::timestamp as created_at,
+    order_id::string as order_id,
+    _fivetran_synced::timestamp as _fivetran_synced
+  from source
+  where coalesce(_fivetran_deleted, false) = false
 )
 
-select * from cleaned
+select * from renamed
