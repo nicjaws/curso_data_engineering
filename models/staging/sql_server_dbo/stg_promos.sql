7bde8e6a827a916592cd772bc05b8741235ebdaa
Nicolas Corbalan <nicolascorbalan@gmail.com>
2025-05-19 17:10:03 +0000

    arreglando el linaje

--- models/staging/sql_server_dbo/stg_promos.sql
+++ models/staging/sql_server_dbo/stg_promos.sql
@@ -1,16 +1,15 @@
+
 {{ config(materialized='view') }}
 
 with source as (
-  select * from alumno24_dev_bronze_db.sql_server_dbo.promos
+  select * from {{ source('sql_server_dbo', 'promos') }}
 ),
 renamed as (
   select
-    md5(cast(coalesce(cast(promo_id as TEXT), '') as TEXT)) as promo_hash_key,
-    CAST(promo_id AS VARCHAR) AS promo_id,
-    CAST(discount AS INTEGER) AS discount,
-    CAST(status AS VARCHAR) AS status,
-    CAST(_fivetran_deleted AS BOOLEAN) AS is_deleted,
-    CAST(_fivetran_synced AS TIMESTAMP) AS fivetran_synced
+    promo_id::string as promo_id,
+    discount::int as discount,
+    status::string as status,
+    _fivetran_synced::timestamp as _fivetran_synced
   from source
   where coalesce(_fivetran_deleted, false) = false
 )
