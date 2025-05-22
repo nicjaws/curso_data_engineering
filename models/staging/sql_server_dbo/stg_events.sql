cb7e0369648112fbde7c66bf762ad972a6813d8d
Nicolas Corbalan <nicolascorbalan@gmail.com>
2025-05-22 08:32:49 +0000

    incremental events

--- models/staging/sql_server_dbo/stg_events.sql
+++ models/staging/sql_server_dbo/stg_events.sql
@@ -1,22 +1,24 @@
-WITH source AS (
-    SELECT * FROM {{ source('sql_server_dbo', 'events') }}
+with source as (
+  select * from {{ source('sql_server_dbo', 'events') }}
+
+  {% if is_incremental() %}
+  where _fivetran_synced > (select max(_fivetran_synced) from {{ this }})
+  {% endif %}
+),
+
+renamed as (
+  select
+    event_id::string as event_id,
+    page_url::string as page_url,
+    event_type::string as event_type,
+    user_id::string as user_id,
+    product_id::string as product_id,
+    session_id::string as session_id,
+    created_at::timestamp as created_at,
+    order_id::string as order_id,
+    _fivetran_synced::timestamp as _fivetran_synced
+  from source
+  where coalesce(_fivetran_deleted, false) = false
 )
 
-SELECT
-    -- Identificadores
-    "EVENT_ID" as event_id,
-    "USER_ID" as user_id,
-    "SESSION_ID" as session_id,
-    "PRODUCT_ID" as product_id,
-    "ORDER_ID" as order_id,
-    
-    -- Datos del evento
-    "PAGE_URL" as page_url,
-    "EVENT_TYPE" as event_type,
-    "CREATED_AT" as created_at,  -- Explícitamente usamos mayúsculas, y lo convertimos a minúsculas
-    
-    -- Metadatos
-    "_FIVETRAN_SYNCED" as fivetran_synced
-
-FROM source
-WHERE "_FIVETRAN_DELETED" = FALSE
+select * from renamed
